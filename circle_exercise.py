2f39f6b5f931105f90045a1c46db07d5345a4d53
Ali Wangara <Aliwangara63@gmail.com>
2025-07-30 14:10:40 +0300

    worked with math operations, pow,sqrt,ceil,floor

--- circle_exercise.py
+++ circle_exercise.py
@@ -1,2 +1,18 @@
 import  math
 
+radius = float(input("Enter the radius of the circle: "))
+
+total  = 2 * math.pi * radius
+total = round(total,2)
+
+print(f"The total area of the circle is: {total}cm")
+
+area = math.pi * pow(radius, 2)
+
+print(f"The total area of the circle is: {round(area,2)}cm")
+
+a =float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+c = math.sqrt(pow(a,2) + pow(b,2))
+
+print(f"The area is {round(c,2)}")
