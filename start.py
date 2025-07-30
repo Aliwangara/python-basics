ff51f352957d6b14b72e24dda9ab37b19a9cc708
Ali Wangara <Aliwangara63@gmail.com>
2025-07-30 12:01:54 +0300

    Leart about striings

--- start.py
+++ start.py
@@ -1,1 +1,10 @@
 print('Welcome To Python');
+
+name = "Ali"
+age = 20
+dev = True
+
+print(type(name))
+print(type(age))
+print(type(dev))
+
---
+++ strings.py
@@ -0,0 +1,8 @@
+#Strings are values inside a quotation mark.
+
+first_Name = "Ali Wangara"
+#To use a string and a variable at the same tme we use f""
+
+
+print(first_Name)
+print(f"Your name is {first_Name}")
