f10801c721d8e0e8148fbea732e862c9eeec1a2c
Ali Wangara <Aliwangara63@gmail.com>
2025-07-30 13:47:40 +0300

    worked with math operations

---
+++ arithmeticoperators_and_mathfunctions.py
@@ -0,0 +1,21 @@
+
+
+friends = 5
+
+# friends+=1
+# friends-=2
+# friends*=3
+# friends/=2
+# friends**=2
+# friends %= 2
+x=3.14
+y=4
+z=5
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4,3)
+# result = max(3,2,1) = 3
+result = min(3,2,1)# = 1
+
+print(result)
