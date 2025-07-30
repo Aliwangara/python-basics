254540dffad9921d1a5727669a4966f8e51e5bd1
Ali Wangara <Aliwangara63@gmail.com>
2025-07-30 12:09:07 +0300

    Learnt integers, floats and booleas

---
+++ Integers.py
@@ -0,0 +1,16 @@
+age  = 25
+quantity = 3
+
+print(f"You are {age} Years Old and you are buying {quantity}items.")
+
+#floats - decimal point numbers.
+
+price = 10.99
+distance = 5.5
+gpa = 3.2
+is_student = True
+
+print(f"the price is $ {price}")
+print(f"the GPA is {gpa}")
+print(f"the distance is {distance}")
+print(f"Are you a  student {is_student}")
