a3be1f551483c815b73ec377f965f04eff8a5d61
Ali Wangara <Aliwangara63@gmail.com>
2025-08-01 21:32:48 +0300

    Learnt string Methods

---
+++ Day3/string_methods.py
@@ -0,0 +1,16 @@
+
+
+name  = input("Enter your name: ")
+print(len(name))
+print(name.find('w'))
+print(name.rfind('a'))
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count('a'))
+print(name.replace('a', 'l'))
+
+name = name.capitalize()
+
+print(name)
