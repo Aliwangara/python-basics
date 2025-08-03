4b7f1edccfe0afba9ddd975bf5497492457c5c57
Ali Wangara <Aliwangara63@gmail.com>
2025-08-03 23:10:27 +0300

    Worked on if statements

--- Day3/while_loops.py
+++ Day3/while_loops.py
@@ -21,22 +21,34 @@
 #     food = input("Enter your favorite food (q to quit): ")
 # print(f"Bye")
 
-num = float(input("Enter a number between 1 and 10 (-1 to quit): "))
+# num = float(input("Enter a number between 1 and 10 (-1 to quit): "))
+#
+# while num < 1 or num > 10 :
+#     print(f"{num} Invalid number")
+#     num = float(input("Enter a number between 1 and 10: "))
+# print(f"Your number is {num}")
+#
+#
+# number  = float(input("Enter a number between 1 and 20: "))
+#
+# while number <10  or number >20:
+#     print(f"{number} Invalid number")
+#     number = float(input("Enter a number between 1 and 20: "))
+# print(f"Your number is {number}")
+#
+# if number >10 or number <20:
+#     print(f"Your number is {number}")
+# else:
+#   num = float(input("enter your number"))
 
-while num < 1 or num > 10 :
-    print(f"{num} Invalid number")
-    num = float(input("Enter a number between 1 and 10: "))
-print(f"Your number is {num}")
+name = input("Enter your name (q to quit): ")
+
+while not name == 'Q':
+    print(f"{name} is incorrect")
+    name = input("Enter your name (q to quit): ")
+
+print(f"Your name is {name}")
 
 
-number  = float(input("Enter a number between 1 and 20: "))
 
-while number <10  or number >20:
-    print(f"{number} Invalid number")
-    number = float(input("Enter a number between 1 and 20: "))
-print(f"Your number is {number}")
 
-if number >10 or number <20:
-    print(f"Your number is {number}")
-else:
-  num = float(input("enter your number"))
