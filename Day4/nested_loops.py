4abdb2f26e6a715d627c97199570fb34f190b583
Ali Wangara <Aliwangara63@gmail.com>
2025-08-05 10:59:33 +0300

    Learnt Nested loops

---
+++ Day4/nested_loops.py
@@ -0,0 +1,9 @@
+# Nested_loop = A loop within another loop(outer,inner)
+
+            # outer_loop:
+            #     inner_loop
+
+for y in range(2):
+    for x in range(1,10):
+        print(x, end="")
+    print()# This prints 1-9 2times  in two different lines
