70e333b94f721f4d8a4d4d54e1fdaf99dd140d2b
Ali Wangara <Aliwangara63@gmail.com>
2025-08-09 20:37:40 +0300

    learnt slicing in python

---
+++ Scrimba/strings-basics-slicing.py
@@ -0,0 +1,16 @@
+msg = "Welcome to this website"
+
+print(msg.count("w"))
+print(msg.lower())
+print(msg.capitalize())
+print(msg.title())
+print(msg.upper())
+
+
+#slicing
+
+print(msg[0]) # gives the first character in a string
+print(msg[-1]) # gives the last character
+print(msg[1:3]) # pricks from character number 1 - 2 : El
+print(msg[2:]) #Picks from  character 2 to last character
+print(msg[:6]) # picks from first character to the fifth character of the text
