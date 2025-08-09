c79bfa911a2a4828786887bcec322283ab8c300a
Ali Wangara <Aliwangara63@gmail.com>
2025-08-09 21:20:41 +0300

    learnt slicing in python

--- Scrimba/strings-basics-slicing.py
+++ Scrimba/strings-basics-slicing.py
@@ -14,3 +14,23 @@ print(msg[-1]) # gives the last character
 print(msg[1:3]) # pricks from character number 1 - 2 : El
 print(msg[2:]) #Picks from  character 2 to last character
 print(msg[:6]) # picks from first character to the fifth character of the text
+
+
+msg='welcome to Python 101: Strings'
+msg1=   msg[18] + " " + msg[:8] + msg[25:29] + msg[7:11] + msg[13] + msg[12] + msg[2] + msg[1] + msg[-5]
+print(msg1.title())
+print(msg1[::-1].title())
+
+# String find/ replace, formatting
+
+msg2 = """Dear ali,You should cut spending
+"""
+
+print(msg2, end="")
+
+msg='Welcome to Python 101: Strings'
+print(msg.find('Python'))
+print(msg.replace('Python', 'Js'))
+
+
+
