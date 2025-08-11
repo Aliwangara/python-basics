82d0a44b08118421abb0c75ceec0e076c2eff1bf
Ali Wangara <Aliwangara63@gmail.com>
2025-08-11 16:41:38 +0300

    Learnt basics of tuples

---
+++ Scrimba/tuples.py
@@ -0,0 +1,12 @@
+#Tuples - faster Lists you can't change
+
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple  =('John','Michael','Terry','Eric','Graham')
+
+# tuples cant perform operations like listeg: append, remove etc
+#but you can access them using indexes eg
+print(friends_tuple[0])
+print(friends_tuple[0:3])
+
+
+print(friends_tuple)
