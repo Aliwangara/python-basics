14593fa1f0f3f035ebdc46563698b498bcbecbcd
Ali Wangara <Aliwangara63@gmail.com>
2025-08-12 09:32:24 +0300

    added a symetric_diffrence function to what I learnt

--- Scrimba/sets.py
+++ Scrimba/sets.py
@@ -4,7 +4,8 @@ my_friends_set = {'Reg','Loretta','Colin','Eric','Graham'}
 
 print(friends_set.intersection(my_friends_set)) # finds similar items in both sets eg: {'Eric', 'Graham'}
 print(friends_set.difference(my_friends_set)) # finds different items in the set
-print(friends_set.union(my_friends_set)) # combines all sets
+print(friends_set.union(my_friends_set))# combines all sets
+print(friends_set.symmetric_difference(my_friends_set)) # finds the items that are unique in each set
 
 
 
