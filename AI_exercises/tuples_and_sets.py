4e28e29447a6771c4694fd1dbf470d530b29afa0
Ali Wangara <Aliwangara63@gmail.com>
2025-09-09 09:53:53 +0300

    did some AI exercises on using: tuples converting them into sets, using map function

--- AI_exercises/tuples_and_sets.py
+++ AI_exercises/tuples_and_sets.py
@@ -1,11 +1,14 @@
-numbers = input("Enter numbers separate them by a space: ")
+u_number = input("Enter couple of numbers leave spaces between them: ")
 
-number_tuple = ()
-number_list = list(number_tuple)
-number_list =','.join(number_list).split(' ')
-number_list.append(numbers)
-print(number_list)
+number_tuple = tuple(map(int, u_number.split()))
 
-# number_tuple = tuple(number_tuple)
+number_set = set(number_tuple)
 
-# print(number_tuple)
+print(number_set)
+
+print("Tuple:", number_tuple)
+print("Set:", number_set)
+print("Length of tuple:", len(number_tuple))
+print("Length of set:", len(number_set))
+
+print(help(map))
